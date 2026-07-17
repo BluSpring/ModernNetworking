42ae03f792d6a5f06eb8a14fc8e52df3c841a8b3
Naz <bluspringindohq@gmail.com>
2026-07-17 14:48:40 +0800

    Add a v2 converter

--- api/src/main/kotlin/xyz/bluspring/modernnetworking/api/NetworkCodec.kt
+++ api/src/main/kotlin/xyz/bluspring/modernnetworking/api/NetworkCodec.kt
@@ -1,6 +1,7 @@
 package xyz.bluspring.modernnetworking.api
 
 import io.netty.buffer.ByteBuf
+import xyz.bluspring.modernnetworking.api.v2.codec.NetworkCodec
 
 @Deprecated(level = DeprecationLevel.ERROR, message = "Refactored to properly match MC 1.20.6.", replaceWith = ReplaceWith("NetworkCodec", "xyz.bluspring.modernnetworking.api.v2.codec.NetworkCodec"))
 data class NetworkCodec<T, B : ByteBuf>(private val encoder: Encoder<B, T>, private val decoder: Decoder<B, T>) {
@@ -10,6 +11,10 @@ data class NetworkCodec<T, B : ByteBuf>(private val encoder: Encoder<B, T>, priv
 
     fun decode(buffer: B): T {
         return this.decoder.decode(buffer)
+    }
+
+    fun asV2(): NetworkCodec<B, T> {
+        return NetworkCodec(this.encoder::encode, this.decoder::decode)
     }
 
     fun interface Encoder<B : ByteBuf, T> {
