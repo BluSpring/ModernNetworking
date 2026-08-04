03cc53f967daa220957779f831c204be1a7d92d3
Naz <bluspringindohq@gmail.com>
2026-08-04 13:29:08 +0800

    delete the template mixin i clearly forgot about

--- common/src/main/java/xyz/bluspring/template/mixin/MinecraftMixin.java
+++
@@ -1,9 +0,0 @@
-package xyz.bluspring.template.mixin;
-
-import org.spongepowered.asm.mixin.Mixin;
-
-import net.minecraft.client.Minecraft;
-
-@Mixin(Minecraft.class)
-public abstract class MinecraftMixin {
-}
